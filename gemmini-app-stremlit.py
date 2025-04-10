432528ba682554716ba324666008f89919849d23
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-04-10 17:22:50 +0530

    llm & template

--- gemmini-app-stremlit.py
+++ gemmini-app-stremlit.py
@@ -7,3 +7,22 @@ from langchain_core.output_parsers import StrOutputParser
 load_dotenv() # Loads the variables from .env file
 
 
+# Now we can instantiate our model object and generate chat completions
+llm = ChatGoogleGenerativeAI(
+    model = "gemmini - 1.5-pro",
+    temperatuere = 0,
+    max_tokens= None,
+    timeout=None,
+    max_retries=2
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a chatbot"),
+        ("Human", "Question : {question}")
+
+    ]
+)
+
+st.title('Langchain Demo with Gemini')
+input_text = st.time_input("Enter your question here")
